6ff99029fe87355f15ac86963391870e91fb1b50
Abdullah Saafan <51491838+Saafan@users.noreply.github.com>
2021-10-14 15:22:53 +0200

    The Basics #2

--- ATourOfCPlusPlus/main.cpp
+++ ATourOfCPlusPlus/main.cpp
@@ -2,21 +2,10 @@
 
 #include "Student.h"
 
-constexpr int Add(int x, int y)
-{
-	return x + y;
-}
 
 int main()
 {
-	int x = 300;
-	int m = 400;
-
-	std::cout << Add(x, m) << std::endl;
-
-
-	for (auto c : { 1,3,4,5,6 })
-		c;
+	std::cout << "(x: " << sizeof(double*) << ")" << std::endl;
 
 	return 0;
 }
