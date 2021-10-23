e6bfaa01c46ea6f4661c243f32ce46259586980a
Abdullah Saafan <51491838+Saafan@users.noreply.github.com>
2021-10-23 23:57:53 +0200

    Classes #4

--- ATourOfCPlusPlus/src/main.cpp
+++ ATourOfCPlusPlus/src/main.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 
+#include <stdio.h>
+
+class Base
+{
+
+public:
+
+	virtual void MustFunction() = 0;
+	void MayFunction();
+
+private:
+
+};
+
+void Base::MayFunction()
+{
+	std::cout << "Base May" << std::endl;
+}
+
+class Derived : public Base
+{
+	void MustFunction();
+};
+
+void Derived::MustFunction()
+{
+	std::cout << "Derived Must" << std::endl;
+}
+
+
 int main()
 {
-	const int x = 100;
-	const int* m = &x;
-	int* y = const_cast<int*>(m);
-	*y = 200;
-
-
-	std::cout << "y: " << &y << " x: "<< &x << std::endl;
+	Base b;
+	Derived d;
+	
+	
+	
 	return 0;
 }
