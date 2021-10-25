58144a9e9249bdc2df7a8a441de2a95e3556d40b
Abdullah Saafan <51491838+Saafan@users.noreply.github.com>
2021-10-25 20:28:15 +0200

    Classes #6

--- ATourOfCPlusPlus/src/main.cpp
+++ ATourOfCPlusPlus/src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-
-#include "Classes.h"
+#include <memory>
 
 int main()
 {
-	Derived d; 
-	Base& b = d;
+	std::unique_ptr<int> ptr = std::make_unique<int>(12);
+	int* x = new int(120);
 
-	b.NormalOverriddenFunction();
-	b.VirtualFunction();
+	std::cout << "Unique Pointer Value: " << *ptr << std::endl;
+	std::cout << "Memory Pointer Value: " << *x << std::endl;
 
+	std::cout << "Smart pointers save the hustle of deleting and deallocating the normal pointers" << std::endl;
+	std::cout << "and at the same time is as efficient and fast as the normal pointers and gives more features, functionality, and information about the pointer." << std::endl;
 	return 0;
 }
